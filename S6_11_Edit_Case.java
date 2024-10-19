6575d0dd089bf0dd9bc982bd7d486e2d42ea0286
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2024-10-19 20:07:23 +0530

    S6_11 - Manoj

--- S6_11_Edit_Case.java
+++ S6_11_Edit_Case.java
@@ -22,11 +22,8 @@ public class S6_11_Edit_Case {
 	@Test
 	public void loginPage() throws InterruptedException, IOException {
 
-		DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setBrowserName("chrome");
-		dc.setPlatform(Platform.LINUX);
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
-		//EdgeDriver driver=new EdgeDriver();             
+		
+		EdgeDriver driver=new EdgeDriver();             
 		driver.get("https://login.salesforce.com/"); 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
