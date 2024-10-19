679c2beed67df03f72392c6a111bd3ade71dfe68
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2024-10-19 19:53:25 +0530

    S6_11 - Manoj

--- S6_11_Edit_Case.java
+++ S6_11_Edit_Case.java
@@ -1,36 +1,59 @@
 package week1.day2;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class S611EditCase {
+public class S6_11_Edit_Case {
 
 	@Test
-	public void loginPage() throws InterruptedException {
-		EdgeDriver driver=new EdgeDriver();             
+	public void loginPage() throws InterruptedException, IOException {
+
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.LINUX);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+		//EdgeDriver driver=new EdgeDriver();             
 		driver.get("https://login.salesforce.com/"); 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
 		driver.findElement(By.id("password")).sendKeys("Leaf$321");
 		driver.findElement(By.id("Login")).click();
-		
+
+		Thread.sleep(1000);
 		//Click on toggle menu button from the left corner
 		driver.findElement(By.xpath("//button[@title='App Launcher']")).click();
 		//Click view All and click Sales from App Launcher
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//button[text()='View All']")).click();
 		driver.findElement(By.xpath("//P[text()='Sales']")).click();
 		//Click on Cases tab visible or select from more.
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//span[text()='More']")).click();
-		
-		driver.executeScript( "arguments[0].click()",driver.findElement(By.xpath("//div[@role='menu']//span[text()='Cases']")));
+		WebElement element=driver.findElement(By.xpath("//span[text()='Cases']"));
+
+		//  Actions action = new Actions(driver);
+		if(element.isDisplayed())
+			driver.executeScript( "arguments[0].click()",element);
+		else {
+			driver.findElement(By.xpath("//span[text()='More']")).click();
+			driver.executeScript( "arguments[0].click()",driver.findElement(By.xpath("//div[@role='menu']//span[text()='Cases']")));
+		}
 		//Click on the Dropdown icon and select Edit from the case you created by reffering 'case owner alias'
 		driver.findElement(By.xpath("//tr[1]/td[7]")).click();
-		
+
 		driver.executeScript( "arguments[0].click()",driver.findElement(By.xpath("//div[@title='Edit']")));
 		//Update Status as Working
 		driver.findElement(By.xpath("//records-record-layout-item[@field-label='Status']//button")).click();
@@ -51,6 +74,6 @@ public class S611EditCase {
 			System.out.println("Working is updated Successfully");
 		else System.out.println("Working is not updated Successfully");
 		driver.close();
-		
+
 	}
 }
