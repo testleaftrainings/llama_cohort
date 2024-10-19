ca8652a45b7c66c6f240e448720db2b72cef9fef
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2024-10-19 20:53:47 +0530

    S6_24 - Manoj

--- S6_24_Edit_Legal_Entity.java
+++ S6_24_Edit_Legal_Entity.java
@@ -1,16 +1,38 @@
 package week2.day1;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class S624EditLegalEntity {
+public class S6_24_Edit_Legal_Entity {
 
 	@Test
-	public void editlegalEntity() throws InterruptedException {
+	public void editlegalEntity() throws InterruptedException, IOException {
+		
+//		DesiredCapabilities dc = new DesiredCapabilities();
+//		dc.setBrowserName("MicrosoftEdge");
+//		dc.setPlatform(Platform.LINUX);
+//		
+//		//dc.
+//		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+//		
+		
 		EdgeDriver driver=new EdgeDriver();             
 		driver.get("https://login.salesforce.com/"); 
 		driver.manage().window().maximize();
@@ -18,6 +40,7 @@ public class S624EditLegalEntity {
 		driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
 		driver.findElement(By.id("password")).sendKeys("Leaf$321");
 		driver.findElement(By.id("Login")).click();
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		
 		//Click on toggle menu button from the left corner
 		driver.findElement(By.xpath("//button[@title='App Launcher']")).click();
@@ -32,15 +55,19 @@ public class S624EditLegalEntity {
 		
 		//Search the Legal Entity 'Salesforce Automation by *Your Name*'
 		driver.findElement(By.xpath("//input[@name='LegalEntity-search-input']")).sendKeys("Salesforce Automation by Manoj");
-		 //Click on the Dropdown icon and Select Edit
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		driver.findElement(By.xpath("//table//tr/td[5]")).click();
+		
+
+        
 		driver.findElement(By.xpath("//ul[@class='scrollable']/li[1]")).click();
 		
 
 		//Enter the Company name as 'Tetsleaf'.
+		driver.findElement(By.xpath("//input[@name='CompanyName']")).clear();
 		driver.findElement(By.xpath("//input[@name='CompanyName']")).sendKeys("Tetsleaf");
 		//Enter Description as 'SalesForce'.
+		driver.findElement(By.xpath("//label[text()='Description']/following::textarea[@class='slds-textarea']")).clear();
 		driver.findElement(By.xpath("//label[text()='Description']/following::textarea[@class='slds-textarea']")).sendKeys("SalesForce");
 		
 		//Select Status as 'Active'
